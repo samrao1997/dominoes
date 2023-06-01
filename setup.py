7e9060cab12906d3054f55d71f37680e373bf0c5
Sam Rao <samrao1997@gmail.com>
2023-06-01 10:34:41 -0700

    Setup package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dominoes',
+    version='6.1.0',
+    description='A Python library for the game of dominoes, with an accompanying CLI and AI players.',
+    url='https://github.com/samrao1997/dominoes',
+    author='Sam Rao',
+    author_email='samrao1997@gmail.com',
+    license='MIT',
+    packages=['dominoes'],
+    test_suite='tests'
+)
